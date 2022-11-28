1c0987a7208484f8915471e4b0ba9f9e357084ed
ekkyarmandi <ekkyarmandi@gmail.com>
2022-11-28 07:19:11 +0700

    added feedback model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=256)
@@ -6,3 +7,13 @@ class Restaurant(models.Model):
     
     def __str__(self) -> str:
         return self.name
+
+class Feedback(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    feedback = models.TextField(default="")
+    stars = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.feedback
