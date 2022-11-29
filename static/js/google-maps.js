74e9e8da9090da8fba2bfa8685ac8a2ac47ae735
ekkyarmandi <ekkyarmandi@gmail.com>
2022-11-29 20:59:18 +0700

    updated google maps api marker icons

--- static/js/google-maps.js
+++ static/js/google-maps.js
@@ -1,19 +1,61 @@
 // Initialize and add the map
-function initMap() {
+function initMap(center, res_coords) {
     // The location of Uluru
-    const uluru = { lat: 40.73, lng: -73.935 };
+    const city = {
+        coord: { lat: center["latitude"], lng: center["longitude"] },
+    }
 
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: uluru,
+        zoom: 14,
+        center: city.coord,
     });
 
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
-    });
+    // Custom icon
+    var icon = {
+        url: "https://cdn-icons-png.flaticon.com/512/6984/6984947.png", // url
+        scaledSize: new google.maps.Size(40, 40), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(20, 40), // anchor
+    };
+
+    // The marker
+    addMarker(city);
+
+    // Place marker
+    for(let i=0; i < res_coords.length; i++) {
+        let coord = res_coords[i]['coordinates']
+        let place = {
+            coord: {
+                lat: coord['latitude'],
+                lng: coord['longitude'],
+            },
+            icon: icon,
+            content: `<h5>${res_coords[i]['name']}</h5>`
+        }
+        addMarker(place);
+    }
+
+    function addMarker(props){
+        let marker = new google.maps.Marker({
+            position: props.coord,
+            map: map,
+        })
+
+        if(props.content){
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+        }
+
+        if(props.icon){
+            marker.setIcon(props.icon);
+        }
+
+        marker.addListener("click",() => {
+            infoWindow.open(map,marker);
+        })
+    }
 }
 
-window.initMap = initMap;
+// window.initMap = initMap;
--- static/js/yelp.js
+++ static/js/yelp.js
@@ -1,77 +1,84 @@
-function getRestaurants() {
-
-    // Get zipcode and radius value
-    var zipcode = document.getElementById("zipcode").value;
-    var radius = document.getElementById("radius").value;
-    if (zipcode) {
-        getYelp(zipcode,radius);
-    } else {
-        alert("zipcode can not be empty!");
-    }
-
-}
-
-function Spinner(){
+function Spinner() {
     let root = document.getElementById("results");
     root.innerHTML = `
-        <div class="spinner-border text-primary" role="status">
+        <div class="spinner-border text-primary mt-5" role="status">
             <span class="visually-hidden">Loading...</span>
         </div>
-    `
+    `;
 }
 
-function fillResults(businesses){
+function fillResults(businesses) {
     let search_result = document.getElementById("search-result");
     let root = document.getElementById("results");
     root.innerHTML = "";
-    if(businesses.length > 0){
-        search_result.innerHTML = `<p class="text-light mt-3">${businesses.length} Restaurants found</p>`
+    if (businesses.length > 0) {
+        search_result.innerHTML = `<p class="text-light mt-3">${businesses.length} restaurants found</p>`;
         businesses.forEach((b) => {
-            let loc = b['location'];
-            let address = `${loc['address1']}, ${loc['city']}, ${loc['state']}, ${loc['country']} ${loc['zip_code']}`;
-            let rating = b['rating'];
-            let reviews = b['review_count'];
+            let loc = b["location"];
+            let address = `${loc["address1"]}, ${loc["city"]}, ${loc["state"]}, ${loc["country"]} ${loc["zip_code"]}`;
+            let rating = b["rating"];
+            let reviews = b["review_count"];
             let item = `
                 <div class="card mt-3 me-3" style="width: 18rem;">
-                    <img class="card-img-top" src="${b['image_url']}" alt="${b['name']}">
+                    <img class="card-img-top" src="${b["image_url"]}" alt="${b["name"]}">
                     <div class="card-body">
-                        <h5 class="card-title"><a class="text-dark text-decoration-none" href="${b['url']}" target="_blank">${b['name']}</a></h5>
+                        <h5 class="card-title"><a class="text-dark text-decoration-none" href="${b["url"]}" target="_blank">${b["name"]}</a></h5>
                         <p class="card-text small mb-0">Rating: ${rating} stars (${reviews.toLocaleString()})</p>
                         <p class="card-text small">${address}</p>
                     </div>
                 </div>
-            `
+            `;
             root.insertAdjacentHTML("beforeEnd", item);
-        })
+        });
     } else {
-        search_result.innerHTML = `<p class="text-light mt-3">No Restaurants within that radius.</p>`
+        search_result.innerHTML = `<p class="text-light mt-3">No restaurants within the radius.</p>`;
     }
 }
 
-function getYelp(zipcode,radius) {
-
+function getYelp(zipcode, radius) {
     // Show the spinner after button pressed
     Spinner();
-    
+
     // Prepare the endpoint and csrf token
     let url = "http://localhost:8000/search-restaurants/";
     let csrf = document.querySelector("input[name=csrfmiddlewaretoken]").value;
 
     let http = new XMLHttpRequest();
     http.open("POST", url, true);
-    http.setRequestHeader("X-CSRFToken",csrf);
-    http.setRequestHeader('Access-Control-Allow-Origin', "*");
-    http.setRequestHeader('Access-Control-Allow-Methods', 'POST');
+    http.setRequestHeader("X-CSRFToken", csrf);
+    http.setRequestHeader("Access-Control-Allow-Origin", "*");
+    http.setRequestHeader("Access-Control-Allow-Methods", "POST");
     http.setRequestHeader("Access-Control-Allow-Headers", "accept, content-type");
     http.setRequestHeader("Access-Control-Max-Age", "1728000");
     http.onreadystatechange = function () {
         if (http.readyState == 4 && http.status == 200) {
+            var after_result = document.querySelector("#search-result + div").getAttribute("id");
+            if (after_result != "map") {
+                let result_div = document.getElementById("search-result");
+                result_div.insertAdjacentHTML("afterEnd", '<div id="map" class="mt-3"></div>');
+            }
+
             let response = JSON.parse(http.responseText);
-            fillResults(response['data']);
+            let restaurants = response['data']['businesses'];
+            fillResults(restaurants);
+
+            let center = response["data"]["region"]["center"];
+            initMap(center,restaurants);
         }
     };
     let formdata = new FormData();
-    formdata.append("zipcode",zipcode);
-    formdata.append("radius",radius);
+    formdata.append("zipcode", zipcode);
+    formdata.append("radius", radius);
     http.send(formdata);
+}
+
+function getRestaurants() {
+    // Get zipcode and radius value
+    var zipcode = document.getElementById("zipcode").value;
+    var radius = document.getElementById("radius").value;
+    if (zipcode) {
+        getYelp(zipcode, radius);
+    } else {
+        alert("zipcode can not be empty!");
+    }
 }
